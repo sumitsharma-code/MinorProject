d9b32846d49a36aa66ab022b10a8711b42adefeb
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2025-03-28 15:13:16 +0530

    added purchase,sales graph

---
+++ graphs/Data.js
@@ -0,0 +1,14 @@
+export const data = [
+  { month: "Jan", Purchase: 55000, Sales: 48000 },
+  { month: "Feb", Purchase: 58000, Sales: 46000 },
+  { month: "Mar", Purchase: 41000, Sales: 50000 },
+  { month: "Apr", Purchase: 35000, Sales: 40000 },
+  { month: "May", Purchase: 40000, Sales: 43000 },
+  { month: "Jun", Purchase: 28000, Sales: 39000 },
+  { month: "Jul", Purchase: 54000, Sales: 30000 },
+  { month: "Aug", Purchase: 34000, Sales: 25000 },
+  { month: "Sep", Purchase: 44000, Sales: 51000 },
+  { month: "Oct", Purchase: 34000, Sales: 18000 },
+  { month: "Nov", Purchase: 34000, Sales: 22000 },
+  { month: "Dec", Purchase: 34000, Sales: 35000 },
+];
